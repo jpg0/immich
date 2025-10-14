99d8a85bd58e0a9c2ae4c142bd2e6340ace51a17
Jonathan Gilbert <jpg@trillica.com>
2025-10-14 18:13:32 +1100

    feat: Add random memories resource

--- open-api/typescript-sdk/src/fetch-client.ts
+++ open-api/typescript-sdk/src/fetch-client.ts
@@ -2857,10 +2857,12 @@ export function reverseGeocode({ lat, lon }: {
 /**
  * This endpoint requires the `memory.read` permission.
  */
-export function searchMemories({ $for, isSaved, isTrashed, $type }: {
+export function searchMemories({ $for, isSaved, isTrashed, order, size, $type }: {
     $for?: string;
     isSaved?: boolean;
     isTrashed?: boolean;
+    order?: MemorySearchOrder;
+    size?: number;
     $type?: MemoryType;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
@@ -2870,6 +2872,8 @@ export function searchMemories({ $for, isSaved, isTrashed, $type }: {
         "for": $for,
         isSaved,
         isTrashed,
+        order,
+        size,
         "type": $type
     }))}`, {
         ...opts
@@ -2893,10 +2897,12 @@ export function createMemory({ memoryCreateDto }: {
 /**
  * This endpoint requires the `memory.statistics` permission.
  */
-export function memoriesStatistics({ $for, isSaved, isTrashed, $type }: {
+export function memoriesStatistics({ $for, isSaved, isTrashed, order, size, $type }: {
     $for?: string;
     isSaved?: boolean;
     isTrashed?: boolean;
+    order?: MemorySearchOrder;
+    size?: number;
     $type?: MemoryType;
 }, opts?: Oazapfts.RequestOpts) {
     return oazapfts.ok(oazapfts.fetchJson<{
@@ -2906,6 +2912,8 @@ export function memoriesStatistics({ $for, isSaved, isTrashed, $type }: {
         "for": $for,
         isSaved,
         isTrashed,
+        order,
+        size,
         "type": $type
     }))}`, {
         ...opts
@@ -4882,6 +4890,11 @@ export enum JobCommand {
     Resume = "resume",
     Empty = "empty",
     ClearFailed = "clear-failed"
+}
+export enum MemorySearchOrder {
+    Asc = "asc",
+    Desc = "desc",
+    Random = "random"
 }
 export enum MemoryType {
     OnThisDay = "on_this_day"
